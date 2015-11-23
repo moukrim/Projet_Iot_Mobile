c192e66c5e8ed7a3328d2073ecc42e8e8d18e463
moukrim <moukrim_yassine@hotmail.fr>
2015-11-23 22:23:37 +0100

    commit 2

--- Projet/www/scripts/index.js
+++ Projet/www/scripts/index.js
@@ -2,26 +2,88 @@
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+function onNotificationGCM(e) {
+    switch (e.event) {
+        case 'registered':
+            if (e.regid.length > 0) {
+                console.log("Regid " + e.regid);
+                alert('registration id = ' + e.regid);
+            }
+            break;
+
+        case 'message':
+            // this is the actual push notification. its format depends on the data     model from the push server
+            alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
+            break;
+
+        case 'error':
+            alert('GCM error = ' + e.msg);
+            break;
+
+        default:
+            alert('An unknown GCM event has occurred');
+            break;
+    }
+}
+
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+    function successHandler(result) {
+        alert('Callback Success! Result = ' + result);
+    }
+
+    function errorHandler(error) {
+        alert(error);
+    }
+
+  
 
     function onDeviceReady() {
+ 
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener('resume', onResume.bind(this), false);
+        
+
+      
+
+        try {
+            var pushNotification = window.plugins.pushNotification;
+            pushNotification.register(successHandler, errorHandler, { "senderID": "967388193865", "ecb": "onNotificationGCM" });
+        } catch (ex) {
+            alert('error: ' + ex);
+        }
+
+
         $(function () {
+            var nom;
+            var nomArduino;
+            function onSuccess(imageData) {
+                $.ajax({
+                    method: "POST",
+                    url: "http://192.168.137.131/Pojet_Iot_Api/php/modifImage.php",
+                    data: { nom: nom, nomArduino: nomArduino, image: "data:image/jpeg;base64," + imageData },
+                }).done(function (msg) {
+
+                    location.reload();
+                });
+            }
+
+            function onFail(message) {
+                alert('Failed because: ' + message);
+            }
         $.get("http://192.168.137.131/Pojet_Iot_Api/php/selectPlante.php", function (data) {
 
            var resultats = $.parseJSON(data);
             $.each(resultats, function (ind, val) {
                 // console.log(val);
-                //$(".circular" + ind).css('background-image', 'url("http://192.168.137.131/Pojet_Iot_Api/"' + val.image + ')');
+              
                 $(".images").append('\
                 <div class="camera" style="float:left; margin-right:10px;">\
                     <div class="circular'+ ind + '">\
-                        <img src="http://192.168.137.131/Pojet_Iot_Api/'+ val.image + '" id="' + val.nomArduino + '" alt="" />\
+                        <img src="'+ val.image + '" id="' + val.nomArduino + '" alt="" />\
                             <p class="nom_plante" id="' + val.nom + '">' + val.nom + '</p>\
                     </div>\
                 </div>\
@@ -29,16 +91,31 @@
 
             });
 
-           $(".camera").click(function () {
-               
-                navigator.camera.getPicture(function (imageUri) {
+            $(".camera").click(function () {
+              
 
-                    $("#text").append("<img src='" + imageUri + "' style='width:50%;' />");
-                }, null, null);
+                nom=$(this).find("p").attr("id");
+                nomArduino=$(this).find("img").attr("id");
+               navigator.camera.getPicture(onSuccess, onFail, {
+                   quality: 75,
+                   destinationType: Camera.DestinationType.DATA_URL,
+                   sourceType: Camera.PictureSourceType.CAMERA,
+                   encodingType: Camera.EncodingType.JPEG,
+                   allowEdit: true,
+                   targetWidth: 90,
+                   targetHeight: 100,
+                   popoverOptions: CameraPopoverOptions,
+                   saveToPhotoAlbum: false
+               });
+                  
+           
+                 
             });
         });
     });
-        
+    
+
+       
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
